ea02af911fb7a1d1b684424a784b20b12805b76c
Frederik Holst <bsb@code-it.de>
2017-12-06 11:06:38 +0100

    Example PHP script for sending notification via e-mail

---
+++ scripts/sendmail.php
@@ -0,0 +1,29 @@
+<?php
+require_once "Mail.php";
+
+$from = 'your@e-mail.de'		// CHANGE THIS!; 
+$to = 'your@e-mail.de'; 		// CHANGE THIS!
+$subject = 'Robotan ' . htmlspecialchars($_GET["error"]); 
+$body = $subject;
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+$smtp = Mail::factory('smtp', array(
+        'host' => 'smtp-server.de',	// CHANGE THIS!
+        'port' => '587',		// You might have to change this
+        'auth' => true,			// You might have to change this
+	'SMTPAuth' => 'tls',		// You might have to change this
+        'username' => 'Username', 	// CHANGE THIS!
+        'password' => 'Passwort' 	// CHANGE THIS!
+    ));
+$mail = $smtp->send($to, $headers, $body);
+
+// Send the mail
+print "Sending mail...<BR>\n";
+$mail = $smtp->send($to, $headers, $body);
+print "Return value: $mail<BR>\nDone.\n";
+?>
